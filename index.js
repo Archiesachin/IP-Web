7d8e3fc9086114c927c853685361703b8537335f
Archie Shah <103347688+Archiesachin@users.noreply.github.com>
2023-08-19 19:28:32 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+//Conditional statements and for loops 
+let n = 5;
+
+function factorial(n) {
+	let ans = 1;
+	
+	if(n === 0)
+		return 1;
+	for (let i = 2; i <= n; i++)
+		ans = ans * i;
+	return ans;
+}
+
+console.log(factorial(n));
